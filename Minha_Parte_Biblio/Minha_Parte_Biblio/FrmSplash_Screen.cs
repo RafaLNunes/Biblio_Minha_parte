cc4c08f949398cb9831d0e545492a28cf78760ad
RafaLNunes <rafaf.code@gmail.com>
2024-10-04 14:51:46 -0300

    Correção feita
    Xuncho
    Gambiarra
    Nunca dar Crtl+shift+f
    sem ter certeza
    tomar cuidado com as marcações

---
+++ Minha_Parte_Biblio/Minha_Parte_Biblio/FrmSplash_Screen.cs
@@ -0,0 +1,54 @@
+namespace Minha_Parte_Biblio
+
+{
+    public partial class FrmSplash_Screen : Form
+    {
+
+        float lbintPorcent = 0;
+        public FrmSplash_Screen()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            lbintPorcent = (100 * PnCharge.Width) / PnBarCharge.MaximumSize.Width;
+            LbVisualPerct.Text = lbintPorcent.ToString() + '%';
+
+            Bar_Charge.Start();
+
+
+            /*
+             *  100 ------- PnBarCharge.MaximumSize.Width
+             *    x ------- PnBarCharge.Width
+             *
+             *   x * PnBarCharge.MaximumSize.Width = 100 * PnBarCharge.Width
+             * 
+             *   x = (100 * PnBarCharge.Width)/PnBarCharge.MaximumSize.Width
+             *  
+             * 
+             */
+        }
+
+        private void Bar_Charge_Tick(object sender, EventArgs e)
+        {
+            while(PnCharge.MaximumSize.Width > PnCharge.Width)
+            {
+                PnCharge.Width += 1;
+                lbintPorcent = 0;
+                    //lbintPorcent = (100 * PnCharge.Width) / PnBarCharge.MaximumSize.Width;
+                    //label1.Text = "";
+                    //label1.Text = lbintPorcent.ToString() + "%";
+            }
+            Bar_Charge.Stop();
+                
+            
+            FrmLogIn logIn = new FrmLogIn();
+            this.Hide();
+            logIn.ShowDialog();
+           
+        }
+
+    }
+}
