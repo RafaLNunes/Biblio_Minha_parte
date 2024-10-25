cd02d2cc85c2e19aefadba05b9f5dc39d12ce57d
RafaLNunes <rafaf.code@gmail.com>
2024-10-25 13:41:38 -0300

    send

--- Minha_Parte_Biblio/Minha_Parte_Biblio/obj/Debug/net8.0-windows/Minha_Parte_Biblio.AssemblyInfo.cs
+++ Minha_Parte_Biblio/Minha_Parte_Biblio/obj/Debug/net8.0-windows/Minha_Parte_Biblio.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Minha_Parte_Biblio")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2f9e9c8bb65850c1eb103a5fddba3da9d4055339")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e9f625cb0c9169d75f4f875a896511a2345f2890")]
 [assembly: System.Reflection.AssemblyProductAttribute("Minha_Parte_Biblio")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Minha_Parte_Biblio")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
